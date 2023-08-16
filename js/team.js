f0abb250ff039aba6129123694d5f1b279113c8a
dangkhoi <khoindd2000@gmail.com>
2023-08-16 19:57:44 +0700

    update team.html and publication.html

---
+++ js/team.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.container_people').hide()
+    $('.container_people[data-id=director]').show()
+    $('#consultant, #director, #member').on('click', function(){
+        $(this).parent().children().removeClass('btn-primary')
+        $(this).parent().children().addClass('btn-outline-primary')
+        $(this).removeClass('btn-outline-primary')
+        $(this).addClass('btn-primary')
+        id = $(this).attr('id')
+        $('.container_people').hide()
+        $('.container_people[data-id=' + id + ']').show()
+        new WOW().init();
+    })
+
+    
+})
